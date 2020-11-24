953f0d8a9d18d7849aa6ffe0902c398d23c476ea
cheslin23t <67148623+cheslin23t@users.noreply.github.com>
2020-11-23 21:27:31 -0500

    Making user-frienndly

---
+++ install.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Installing needed files, Please make sure you installed Nodejs from https://nodejs.org/dist/v14.15.1/node-v14.15.1-x64.msi (Hold ctrl while clicking link)
+pause
+echo If you have not, close this window and right click install.bat and click Run with administrator
+pause
+cd installer
+start installer.bat
+echo You may now close this window
+pause
